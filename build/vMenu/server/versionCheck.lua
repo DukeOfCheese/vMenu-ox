42a0f4739c04ad1155428de68ccbaddfc383d8f7
JoeV2 <146123038+Joe-Development@users.noreply.github.com>
2025-11-15 23:42:12 -0800

    (refactor/fix): refactors some code styles, n more
    i fixed an indexing issue for Customization, also refactored some code styling, and fixed it so it points to "DukeOfCheese/vMenu-ox" github repo

--- build/vMenu/server/versionCheck.lua
+++ build/vMenu/server/versionCheck.lua
@@ -1,9 +1,9 @@
 CreateThread(function()
-    lib.versionCheck("Gravxd/vMenu-ox")
+    lib.versionCheck("DukeOfCheese/vMenu-ox")
 
-    if Config.EnableServerList then
-        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
-        SetConvarServerInfo("Framework", ("vMenu-ox - %s"):format(currentVersion))
-        lib.print.info("vMenu-ox loaded! Version: " .. currentVersion)
-    end
+    if not Config.EnableServerList then return end
+
+    local currentVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+    SetConvarServerInfo("Framework", ("vMenu-ox - %s"):format(currentVersion))
+    lib.print.info("vMenu-ox loaded! Version: " .. currentVersion)
 end)
