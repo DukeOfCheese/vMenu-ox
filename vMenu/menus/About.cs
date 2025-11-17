fb9b48a7f0e6c3e2889c00bba1f7c223c9be1a3c
DukeOfCheese <81943364+DukeOfCheese@users.noreply.github.com>
2025-11-17 11:50:47 +1100

    (add): Pro-developers to about menu

--- vMenu/menus/About.cs
+++ vMenu/menus/About.cs
@@ -17,7 +17,7 @@ namespace vMenuClient.menus
             {
                 Label = $"~h~{MainMenu.Version}~h~"
             };
-            var credits = new MenuItem("About vMenu / Credits", "vMenu is made by ~b~Vespura~s~.\n\nThis version is a fork designed by ~g~Grav~s~ to work with ox_lib and integrate other external features.\n\nFor more info, check out: ~b~github.com/Gravxd/vMenu-ox~s~.\n\nOriginal Project: ~b~vespura.com/vmenu~s~\n\nThank you to: Deltanic, Brigliar, IllusiveTea, Shayan Doust, zr0iq and Golden for your contributions!");
+            var credits = new MenuItem("About vMenu / Credits", "vMenu is made by ~b~Vespura~s~.\n\nThis version is a fork designed by ~g~Grav~s~, ~y~DukeOfCheese~s~ and ~p~JoeV2~s~ to work with ox_lib and integrate other external features.\n\nFor more info, check out: ~b~github.com/Gravxd/vMenu-ox~s~.\n\nOriginal Project: ~b~vespura.com/vmenu~s~\n\nThank you to: Deltanic, Brigliar, IllusiveTea, Shayan Doust, zr0iq and Golden for your contributions!");
 
             var serverInfoMessage = vMenuShared.ConfigManager.GetSettingsString(vMenuShared.ConfigManager.Setting.vmenu_server_info_message);
             if (!string.IsNullOrEmpty(serverInfoMessage))
