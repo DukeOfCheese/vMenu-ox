a4d8a80b87c6b80042b40850d58afe2d69d4ece6
DukeOfCheese <81943364+DukeOfCheese@users.noreply.github.com>
2026-01-12 14:03:58 +1100

    (fix): Push up version number

--- build/vMenu/fxmanifest.lua
+++ build/vMenu/fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'bodacious'
 games { 'gta5' }
 
 description 'vMenu Fork - github.com/DukeOfCheese/vMenu-ox'
-version '2.0.0'
+version '2.1.0'
 author 'Tom Grobbe (vMenu), Gravxd & DukeOfCheese (vMenu-ox)'
 ui_page 'storage.html'
 
